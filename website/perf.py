bed9c267176251d9ab5c52048b9ee5cbcdab096e
stelaninja <stefan@johansson.com>
2022-01-30 19:24:57 +0100

    Added take off distances

--- website/perf.py
+++ website/perf.py
@@ -8,14 +8,20 @@ def get_aircraft_data(aircraft_type):
     with open("./aircraft/da40.json", "r") as f:
         data = json.load(f)
 
-    ground_rolls = np.array(data[aircraft_type]["landing"]["ground_rolls"]).astype(
+    land_ground_rolls = np.array(data[aircraft_type]["landing"]["ground_rolls"]).astype(
         float
     )
-    over50 = np.array(data[aircraft_type]["landing"]["land_dists"]).astype(float)
+    land_over50 = np.array(data[aircraft_type]["landing"]["over50"]).astype(float)
+    to_ground_rolls = np.array(data[aircraft_type]["takeoff"]["ground_rolls"]).astype(
+        float
+    )
+    to_over50 = np.array(data[aircraft_type]["takeoff"]["over50"]).astype(float)
 
     # Replace -1 values with np.nan
-    ground_rolls[ground_rolls == -1] = np.nan
-    over50[over50 == -1] = np.nan
+    land_ground_rolls[land_ground_rolls == -1] = np.nan
+    land_over50[land_over50 == -1] = np.nan
+    to_ground_rolls[to_ground_rolls == -1] = np.nan
+    to_over50[to_over50 == -1] = np.nan
 
     weights = np.array(data[aircraft_type]["weights"]).astype(int)
 
@@ -30,42 +36,86 @@ def get_aircraft_data(aircraft_type):
     OAT_range = range(*oat_data)
     PA_range = range(*pa_data)
 
-    return OAT_range, PA_range, ground_rolls, over50, weights
+    return (
+        OAT_range,
+        PA_range,
+        to_ground_rolls,
+        to_over50,
+        land_ground_rolls,
+        land_over50,
+        weights,
+    )
 
 
 # Function to interpolate landing distance from table
-def get_landing_distance(pa, oat, law, ac_type):
+def get_distances(land_pa, to_pa, land_oat, to_oat, law, tow, ac_type):
     # Get the aircraft data
-    OAT_range, PA_range, ground_rolls, over50, weights = get_aircraft_data(ac_type)
+    (
+        OAT_range,
+        PA_range,
+        to_ground_rolls,
+        to_over50,
+        land_ground_rolls,
+        land_over50,
+        weights,
+    ) = get_aircraft_data(ac_type)
+
+    # Check if weights are less than minimum in chart and if so set them to minimum in chart
+    if law < min(weights):
+        law = min(weights)
+    if tow < min(weights):
+        tow = min(weights)
 
     # Create the xarray tables
     land_obst = xr.DataArray(
-        over50,
+        land_over50,
         dims=("LAW", "PA", "OAT"),
         coords={"PA": PA_range, "OAT": OAT_range, "LAW": weights},
     )
-    gr = xr.DataArray(
-        ground_rolls,
+    land_gr = xr.DataArray(
+        land_ground_rolls,
         dims=("LAW", "PA", "OAT"),
         coords={"PA": PA_range, "OAT": OAT_range, "LAW": weights},
+    )
+    to_obst = xr.DataArray(
+        to_over50,
+        dims=("TOW", "PA", "OAT"),
+        coords={"PA": PA_range, "OAT": OAT_range, "TOW": weights},
+    )
+    to_gr = xr.DataArray(
+        to_ground_rolls,
+        dims=("TOW", "PA", "OAT"),
+        coords={"PA": PA_range, "OAT": OAT_range, "TOW": weights},
     )
 
     # Try to interpolate values
+    # print(to_gr)
     try:
-        land_dist = round(float(land_obst.interp(PA=pa, OAT=oat, LAW=law).values), 2)
-        ground_roll = round(float(gr.interp(PA=pa, OAT=oat, LAW=law).values), 2)
+        land_dist = round(
+            float(land_obst.interp(PA=land_pa, OAT=land_oat, LAW=law).values), 2
+        )
+        land_ground_roll = round(
+            float(land_gr.interp(PA=land_pa, OAT=land_oat, LAW=law).values), 2
+        )
+        to_dist = round(float(to_obst.interp(PA=to_pa, OAT=to_oat, TOW=tow).values), 2)
+        to_ground_roll = round(
+            float(to_gr.interp(PA=to_pa, OAT=to_oat, TOW=tow).values), 2
+        )
     except ValueError as e:
         if str(e) == "cannot convert float NaN to integer":
-            return f"Values outside chart range for {pa=} and {oat=}", None
+            return f"Values outside chart range.", None, None, None
         else:
-            return e, None
+            return e, None, None, None
 
     # Check if values are outside range of chart
-    if np.isnan(land_dist) or np.isnan(ground_roll):
-        return f"Values outside chart range for {pa=} and {oat=}", None
+    if np.isnan(land_dist) or np.isnan(to_ground_roll):
+        print("=" * 50, "ERROR HERE", "=" * 50)
+        print(f"{to_pa=}, {to_oat=}, {tow=}")
+        print(f"{land_dist=}, {to_ground_roll=}")
+        return f"Values outside chart range.", None, None, None
 
-    return land_dist, ground_roll
+    return land_dist, land_ground_roll, to_dist, to_ground_roll
 
 
 if __name__ == "__main__":
-    print(get_landing_distance(10000, 30, 1310, "da40_ng"))
+    print(get_distances(0, 60, 15, 0, 1310, 1310, "da40_ng"))
--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
-from .perf import get_landing_distance
+from .perf import get_distances
 
 views = Blueprint("views", __name__)
 
@@ -8,47 +8,61 @@ views = Blueprint("views", __name__)
 def home():
     # Get input variables from form
     if request.method == "POST":
-        pa = request.form.get("press_alt")
-        oat = request.form.get("oat")
-        weight = request.form.get("weight")
+        land_pa = request.form.get("land_press_alt")
+        land_oat = request.form.get("land_oat")
+        land_weight = request.form.get("land_weight")
+        to_pa = request.form.get("to_press_alt")
+        to_oat = request.form.get("to_oat")
+        to_weight = request.form.get("to_weight")
 
         # Check if input is numbers
         try:
-            pa = int(pa)
-            oat = int(oat)
-            weight = int(weight)
+            land_pa = int(land_pa)
+            land_oat = int(land_oat)
+            land_weight = int(land_weight)
+            to_pa = int(to_pa)
+            to_oat = int(to_oat)
+            to_weight = int(to_weight)
         except:
-            pa = 0
-            oat = 15
+            land_pa, to_pa = 0, 0
+            land_oat, to_oat = 15, 15
+            land_weight, to_weight = 1310, 1310
             flash("Invalid Pressure altitude or temperature", category="error")
 
     # Default values when loading page
     else:
-        pa = 0
-        oat = 15
-        weight = 1310
+        land_pa, to_pa = 0, 0
+        land_oat, to_oat = 15, 15
+        land_weight, to_weight = 1310, 1310
 
     # Set aircraft type
     ac_type = "da40_ng"
 
     # Calculate distances
-    ld, gr = get_landing_distance(pa, oat, weight, ac_type)
+    ld, lgr, tod, togr = get_distances(
+        land_pa, to_pa, land_oat, to_oat, land_weight, to_weight, ac_type
+    )
 
     # Check if calculation returned error
-    if isinstance(ld, str):
-        flash(ld, category="error")
+    if isinstance(tod, str) or any(v is None for v in [tod, togr, ld, lgr]):
+        flash("An error occurred", category="error")
         pa = 0
         oat = 15
         # ld, gr = get_landing_distance(pa, oat)
-        ld, gr = "ERROR", "ERROR"
+        ld, lgr, tod, togr = "ERROR", "ERROR", "ERROR", "ERROR"
 
     # Render the page
     return render_template(
         "home.html",
-        pa=pa,
-        oat=oat,
+        land_pa=land_pa,
+        land_oat=land_oat,
         landing_dist=ld,
-        ground_roll=gr,
-        weight=weight,
+        land_ground_roll=lgr,
+        land_weight=land_weight,
+        to_pa=to_pa,
+        to_oat=to_oat,
+        tod=tod,
+        togr=togr,
+        to_weight=to_weight,
         ac_type=ac_type,
     )
